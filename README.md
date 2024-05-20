ad84ae94793218c46348017830d26e3d89b193e4
Pierre Filliolaud <git-dev@filliolaud.net>
2024-05-20 10:40:56 +0200

    Working version not accurate

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup
+poetry install
+
+# Run
+poetry run python ./neuro_py_double_net/neurodouble.py
---
+++ neuro_py_double_net/neurodouble.py
@@ -0,0 +1,33 @@
+from keras import layers
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+
+# units = nb neuro
+# input
+# Define model layers
+model.add(layers.Dense(units=3, input_shape=[1]))
+model.add(layers.Dense(units=64))
+model.add(layers.Dense(units=1))
+
+#Input/Output
+input_data = [1,2,3,4,5]
+output_data = [2,4,6,8,10]
+
+# Convert list to numpy arrays
+x = np.array(input_data)
+y = np.array(output_data)
+
+# Compile and train model
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x=x,y=y,epochs=1000)
+
+# Prediction loop
+while True:
+    z = int(input('Number: '))
+    z_array = np.array([[z]])  # Convert to numpy array
+    prediction = model.predict(z_array)
+    print('Output: ' + str(prediction[0][0]))  # Display prediction
